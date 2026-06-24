b9456c4de9adab6a231ba2eef8ac5f72c6f8a942
Akash <gangwarakash555@gmail.com>
2026-06-24 07:34:45 +0530

    fixing error

--- app.js
+++ app.js
@@ -127,6 +127,4 @@ app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Something went wrong" } = err;
     res.status(statusCode).render("error.ejs", { message });
 });
-app.listen(8080, () => {
-  console.log("server is listening to port 8080");
-});
+
