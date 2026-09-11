d84a331b439c13da065bc617f26dd7a11f65a5f1
Akash Gangwar <gangwarakash555@gmail.com>
2026-09-11 23:47:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://make-your-trip-90sr.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
